666833771ffd0a818c1d6a971e3983c241b960dd
dennis-kap <102987428+dennis-kap@users.noreply.github.com>
2023-04-01 14:00:45 -0400

    Added login test

---
+++ tests/unit/test_login.py
@@ -0,0 +1,38 @@
+import os
+import pytest
+from flask import Flask
+
+from app import create_app, app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_login_screen():
+    flask_app = create_app('flask_test.cfg')
+
+    response = flask_app.test_client().get('/login')
+
+    assert response.status_code == 200
+    assert b"Don't have an account yet?" in response.data
+
+def test_login():
+    
+    with app.test_client() as client:
+
+        response = client.post('/login', data=dict(username='Email@email.com', password='Test123'))
+
+        print(response.location)
+
+        # assert response.status_code == 200
+        assert b"Portfolio" in response.data
+        # assert response.location == 'http://127.0.0.1:5000/home'
+
+def test_incorrect_login():
+    with app.test_client() as client:
+    
+        response = client.post('/login', data=dict(username='wrongUser', password='wrongPass'))
+
+        assert response.status_code == 200
+        assert b"Invalid username or password" in response.data
