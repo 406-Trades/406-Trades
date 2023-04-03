b90f14d43d60c21a5f7cf97863215c36eb75ba5a
dennisicecap1 <102987428+dennis-kap@users.noreply.github.com>
2023-04-02 23:27:31 -0400

    uh

--- tests/functional/test_buy_sell.py
+++ tests/functional/test_buy_sell.py
@@ -16,6 +16,8 @@ app.secret_key = '406-trades'
 
 transaction = Transaction()
 app.register_blueprint(transaction.home_buy_blueprint)
+app.register_blueprint(transaction.buy_stock_blueprint)
+app.register_blueprint(transaction.sell_stock_blueprint)
 
 api = tradeapi.REST(config.API_KEY, config.SECRET_KEY, "https://paper-api.alpaca.markets/")
 headers = {'APCA-API-KEY-ID': config.API_KEY, 'APCA-API-SECRET-KEY': config.SECRET_KEY}
@@ -36,9 +38,9 @@ def client():
 
         yield client
 
-# Testing market search with a valid stock
-def test_home_buy_ardx(client):
-    aSymbol = 'ARDX'
+# Testing stock purchase from home
+def test_home_buy_sq(client):
+    aSymbol = 'SQ'
 
     with client.session_transaction() as session:
         acc = accounts.find_one({'username': session.get('username')})
@@ -60,8 +62,48 @@ def test_home_buy_ardx(client):
             assert bytes(aSymbol, encoding='utf8') in response.data
 
 
-def test_buy():
-    pass
+# Testing stock purchase from search
+def test_buy(client):
+    aSymbol = 'SQ'
 
-def test_sell():
-    pass
+    with client.session_transaction() as session:
+        acc = accounts.find_one({'username': session.get('username')})
+
+        with app.test_request_context():         
+            bal = acc['balance']
+
+            flask_app = create_app('flask_test.cfg')
+            response = flask_app.test_client().post('/buy_stock?username=Email@email.com&symbol={}&from=search'.format(aSymbol), data={'search-amount':1})
+
+            get_price = round(api.get_latest_trade(aSymbol).price, 2)
+            expectedBal = bal - get_price
+            
+            acc = accounts.find_one({'username': session.get('username')})
+            newBal = acc['balance']
+
+            assert response.status_code == 200
+            assert expectedBal == newBal
+            assert bytes(aSymbol, encoding='utf8') in response.data
+
+# Testing stock sale from search
+def test_sell(client):
+    aSymbol = 'SQ'
+
+    with client.session_transaction() as session:
+        acc = accounts.find_one({'username': session.get('username')})
+
+        with app.test_request_context():         
+            bal = acc['balance']
+
+            flask_app = create_app('flask_test.cfg')
+            response = flask_app.test_client().post('/sell_stock?username=Email@email.com&symbol={}&from=search'.format(aSymbol), data={'search-amount':2})
+
+            get_price = round(api.get_latest_trade(aSymbol).price, 2)
+            expectedBal = bal + (get_price * 2)
+            
+            acc = accounts.find_one({'username': session.get('username')})
+            newBal = acc['balance']
+
+            assert response.status_code == 200
+            assert expectedBal == newBal
+            assert bytes(aSymbol, encoding='utf8') in response.data
