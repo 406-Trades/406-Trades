e0524e333f70480bb83e3a9061837eccb88e8837
Nikita Berezyuk ☭ <96497560+berezucc@users.noreply.github.com>
2024-02-18 14:52:23 -0500

    tests

---
+++ tests/test_account_settings.py
@@ -0,0 +1,77 @@
+import os
+import pytest
+from flask import Flask, session
+from bs4 import BeautifulSoup
+import pymongo
+from pymongo import MongoClient
+from unittest.mock import patch
+import time
+
+# Accounts DB
+cluster = MongoClient("mongodb+srv://Abhari:Abhari@cluster0.pqgawmw.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["406-Trades"]
+accounts = db["Accounts"]
+
+from app import create_app, app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_logout():
+     with app.test_client() as client:
+        client.post('/login', data=dict(username='Email@email.com', password='Test1234'))
+        response = client.post('/logout')
+        assert 'username' not in session
+
+def test_change_user():
+    with app.test_client() as client:
+        client.post('/create', data=dict(username='4@gmail.com', password='Abc123', passwordTwo='Abc123'))
+        client.post('/login', data=dict(username='4@gmail.com', password='Abc123'))
+        acc = accounts.find_one({'username': '4@gmail.com'})
+        print(acc)
+        response = client.post('/account_settings?c=changeUsr&oldUser=4@gmail.com', data=dict(username='42@gmail.com'))
+        updated_acc = accounts.find_one({'username': '42@gmail.com'})
+        print(updated_acc)
+        assert b'Account Settings' in response.data
+        assert updated_acc
+        client.post('/del_account')
+
+def test_change_user_invalid():
+    with app.test_client() as client:
+        client.post('/create', data=dict(username='4@gmail.com', password='Abc123', passwordTwo='Abc123'))
+        client.post('/login', data=dict(username='4@gmail.com', password='Abc123'))
+        acc = accounts.find_one({'username': '4@gmail.com'})
+        print(acc)
+        response = client.post('/account_settings?c=changeUsr', data=dict(username='ABC'))
+        updated_acc = accounts.find_one({'username': 'ABC'})
+        print(updated_acc)
+        assert b'Invalid Username, must be a valid email address' in response.data
+        assert updated_acc is None
+        client.post('/del_account')
+
+def test_change_password():
+    with app.test_client() as client:
+        with client.session_transaction() as session:
+            session['username'] = '4@gmail.com'
+        client.post('/create', data=dict(username='4@gmail.com', password='Abc123', passwordTwo='Abc123'))
+        client.post('/login', data=dict(username='4@gmail.com', password='Abc123'))
+        response = client.post('/account_settings?c=changePass&oldUser=4@gmail.com', data=dict(password='Abc123', passwordTwo='New123'))
+        assert b'Account Settings' in response.data
+        acc = accounts.find_one({'username': '4@gmail.com'})
+        assert acc['password'] == 'New123'
+        client.post('/del_account')
+
+def test_change_password_invalid():
+    with app.test_client() as client:
+        with client.session_transaction() as session:
+            session['username'] = '4@gmail.com'
+        client.post('/create', data=dict(username='4@gmail.com', password='Abc123', passwordTwo='Abc123'))
+        client.post('/login', data=dict(username='4@gmail.com', password='Abc123'))
+        response = client.post('/account_settings?c=changePass', data=dict(password='Abc123', passwordTwo='123'))
+        assert b'Account Settings' in response.data
+        acc = accounts.find_one({'username': '4@gmail.com'})
+        assert acc['password'] == 'Abc123'
+        assert b'Invalid Password: Password must contain 1 lowercase, 1 uppercase, and 1 number' in response.data
+        client.post('/del_account')
---
+++ tests/test_admin.py
@@ -0,0 +1,75 @@
+import os
+import pytest
+from flask import Flask
+from bs4 import BeautifulSoup
+import pymongo
+from pymongo import MongoClient
+from unittest.mock import patch
+import time
+
+# Accounts DB
+cluster = MongoClient("mongodb+srv://Abhari:Abhari@cluster0.pqgawmw.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["406-Trades"]
+accounts = db["Accounts"]
+
+from app import create_app, app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_admin_login():
+    with app.test_client() as client:
+        response = client.post('/login', data=dict(username='admin', password='admin'))
+        assert response.status_code == 302
+        assert response.location == '/admin'
+
+def test_admin_page_incorrect():
+    with app.test_client() as client:
+        client.post('/login', data=dict(username='nikita2@gmail.com', password='Abc123'))
+        response = client.post('/admin')
+        assert response.status_code != 200
+
+def test_admin_stock_auth():
+    with app.test_client() as client:
+        client.post('/login', data=dict(username='admin', password='admin'))
+        response = client.post('/stock_authenticate', data=dict(stock='AAPL'))
+        assert b"is a valid stock." in response.data
+
+def test_admin_invalid_stock_auth():
+    with app.test_client() as client:
+        client.post('/login', data=dict(username='admin', password='admin'))
+        response = client.post('/stock_authenticate', data=dict(stock='AAPL123123'))
+        assert b"does not exist." in response.data
+
+def test_admin_account_auth():
+    with app.test_client() as client:
+        client.post('/login', data=dict(username='admin', password='admin'))
+        response = client.post('/account_authenticate', data=dict(account='Email@email.com'))
+        assert b"Email@email.com exists in the database!" in response.data
+
+def test_admin_invalid_account_auth():
+    with app.test_client() as client:
+        client.post('/login', data=dict(username='admin', password='admin'))
+        response = client.post('/account_authenticate', data=dict(account='asd'))
+        assert b"asd does not exist in the database." in response.data
+
+def test_admin_delete_account():
+    with app.test_client() as client:
+        client.post('/create', data=dict(username='1@gmail.com', password='Abc123', passwordTwo='Abc123'))
+        client.post('/login', data=dict(username='1@gmail.com', password='Abc123'))
+        client.post('/login', data=dict(username='admin', password='admin'))
+        acc = accounts.find_one({'username': '1@gmail.com'})
+        response = client.post('/edit_account?username=1@gmail.com&id={}'.format(str(acc['_id'])), data=dict(username='1@gmail.com', submit='Delete'))
+        assert response.status_code == 200
+        assert accounts.find_one({'username': '1@gmail.com'}) is None
+    
+def test_admin_edit_account():
+    with app.test_client() as client:
+        client.post('/create', data=dict(username='5@gmail.com', password='Abc123', passwordTwo='Abc123'))
+        client.post('/login', data=dict(username='admin', password='admin'))
+        acc = accounts.find_one({'username': '5@gmail.com'})
+        response = client.post('/edit_account?id={}'.format(str(acc['_id'])), data=dict(username='5@gmail.com', investments=str(acc['investments']), stocks=str(acc['stocks']), saved=str([]), balance='123', submit='Save'))
+        updated_acc = accounts.find_one({'username': '5@gmail.com'})
+        assert updated_acc['balance'] == 123.0
